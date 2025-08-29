140a688cc03e385675fda9bf6deaab6ec80b9fbf
Prashay <113265973+wadesword89@users.noreply.github.com>
2025-08-29 09:26:40 -0700

    add mhm and ads api

---
+++ api/data_sources/prism_api.py
@@ -0,0 +1,118 @@
+import requests
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+
+def requestPrismDepthData(startTime: str, endTime: str):
+    """
+    Fetch FM depth data from ADS PRISM API.
+
+    Args:
+        startTime: ISO 8601 formatted datetime string (e.g. '2025-03-01T00:00:00')
+        endTime: ISO 8601 formatted datetime string (e.g. '2025-03-01T23:59:59')
+
+    Returns:
+        Dict containing the API response data"
+    """
+
+    startArr = startTime.split(":")
+    endArr = endTime.split(":")
+    PRISM_API_TOKEN = os.getenv("NEXT_PUBLIC_PRISM_API_TOKEN")
+
+    headers = {
+        "accept": "text/plain",
+        "x-ads-dev": PRISM_API_TOKEN,  # API key for FY2425 data
+    }
+
+    entityId = 4122  # Use 4122 to get DEPTH, or 4405 to get WATERTEMP_1
+
+    # locationId: 2 -> 7 = "ALB_0212A_001" -> "ALB_0212A_006"
+
+    url = f"https://api.adsprism.com/api/Telemetry?locationId=2&locationId=3&locationId=4&locationId=5&locationId=6&locationId=7&entityId={entityId}&start={startArr[0]}%3A{startArr[1]}%3A{startArr[2]}&end={endArr[0]}%3A{endArr[1]}%3A{endArr[2]}"
+
+    response = requests.get(url, headers=headers)
+    data = response.json()
+
+    return data
+
+
+start = '2025-03-01T00:00:00'
+end = '2025-03-01T00:59:59'
+result = requestPrismDepthData(start, end)
+print(result)
+
+
+def requestPrismTempData(startTime: str, endTime: str):
+    """
+    Fetch FM wastewater temp data from ADS PRISM API.
+
+    Args:
+        startTime: ISO 8601 formatted datetime string (e.g. '2025-03-01T00:00:00')
+        endTime: ISO 8601 formatted datetime string (e.g. '2025-03-01T23:59:59')
+
+    Returns:
+        Dict containing the API response data"
+    """
+
+    startArr = startTime.split(":")
+    endArr = endTime.split(":")
+
+    headers = {
+        "accept": "text/plain",
+        "x-ads-dev": "pN64fDCNXfEWHgj8+KPRp7QPoJIPM2ONcOjPo92EoQI=",  # API key for FY2425 data
+    }
+
+    entityId = 4405  # Use 4122 to get DEPTH, or 4405 to get WATERTEMP_1
+
+    # locationId: 2 -> 7 = "ALB_0212A_001" -> "ALB_0212A_006"
+
+    url = f"https://api.adsprism.com/api/Telemetry?locationId=2&locationId=3&locationId=4&locationId=5&locationId=6&locationId=7&entityId={entityId}&start={startArr[0]}%3A{startArr[1]}%3A{startArr[2]}&end={endArr[0]}%3A{endArr[1]}%3A{endArr[2]}"
+
+    response = requests.get(url, headers=headers)
+    data = response.json()
+
+    return data
+
+
+# start = '2025-03-01T00:00:00'
+# end = '2025-03-01T23:59:59'
+# result = requestPrismTempData(start, end)
+# print(result)
+
+
+def requestPrismRainData(startTime: str, endTime: str):
+    """
+    Fetch Rain Guage 11 data from ADS PRISM API.
+
+    Args:
+        startTime: ISO 8601 formatted datetime string (e.g. '2025-03-01T00:00:00')
+        endTime: ISO 8601 formatted datetime string (e.g. '2025-03-01T23:59:59')
+
+    Returns:
+        Dict containing the API response data"
+    """
+
+    startArr = startTime.split(":")
+    endArr = endTime.split(":")
+    entityId = 2123  # RAIN
+    locationId = 22  # RG11
+
+    headers = {
+        "accept": "text/plain",
+        "x-ads-dev": "9vRkqNrr9PmloH8pEGcwj70e8OXsoZ/Nhd+EC8zzp9o=",  # API key for FY2425 data
+    }
+
+    rainUrl = f"https://api.adsprism.com/api/Telemetry?locationId={locationId}&entityId={entityId}&start={startArr[0]}%3A{startArr[1]}%3A{startArr[2]}&end={endArr[0]}%3A{endArr[1]}%3A{endArr[2]}"
+
+    response = requests.get(rainUrl, headers=headers)
+    data = response.json()
+
+    return data
+
+
+# start = '2025-03-01T00:00:00'
+# end = '2025-03-01T23:59:59'
+# result = requestPrismRainData(start, end)
+# print(result)
--- api/index.py
+++ api/index.py
@@ -1,5 +1,6 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException, Request
 from fastapi.middleware.cors import CORSMiddleware
+from api.data_sources.prism_api import requestPrismDepthData
 
 app = FastAPI(docs_url="/api/py/docs", openapi_url="/api/py/openapi.json")
 
@@ -15,3 +16,25 @@ app.add_middleware(
 def hello_fast_api():
     return {"message": "Hello from FastAPI"}
 
+# Get Flow Meter Data (From PRISM API)
+@app.post("/api/py/prism_api")
+async def prism_api(request: Request):
+    try:
+        body = await request.json()
+        startTime = body.get("startTime")
+        endTime = body.get("endTime")
+
+        if not startTime or not endTime:
+            raise HTTPException(
+                status_code=400, detail="startTime and endTime are required"
+            )
+
+        # PRISM DATA
+        result = requestPrismDepthData(startTime, endTime)
+
+        if not result:
+            raise HTTPException(status_code=404, detail="Data not found in PRISM API")
+        return result
+
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
